293bfa6469fd5bccfea92ec745fc288262e368d2
RustemYeldessov <eldesov1997@gmail.com>
2025-06-10 20:45:08 +0500

    Исправил ошибку в db.py

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,6 +1,15 @@
 from psycopg import errors as pg_errors
+from flask import flash
 
-try:
-    cursor.execute(query, params)
-except pg_errors.UniqueViolation:
-    flash('URL уже существует', 'warning')
+
+def execute_query(connection, query, params=None):
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(query, params or ())
+            return cursor
+    except pg_errors.UniqueViolation:
+        flash('URL уже существует', 'danger')
+        return None
+    except pg_errors.Error as e:
+        flash(f'Ошибка базы данных: {e}', 'danger')
+        return None

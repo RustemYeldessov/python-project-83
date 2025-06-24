fc5f7487790ee70617f900484279c1c074ebd1b1
RustemYeldessov <eldesov1997@gmail.com>
2025-06-24 21:04:16 +0500

    refactor: app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -57,7 +57,7 @@ def add_url():
 
     if not is_valid_url(normal_url):
         flash('Некорректный URL', 'danger')
-        return redirect(url_for('index'))
+        return redirect(url_for('urls'))
 
     conn = db.connect_database(app)
     existed_url = db.check_url_exists(conn, normal_url)
